05ad7f191686b601a2a53ed6966def8a6272eec9
Ashwin Sundar <assundar@paypal.com>
2016-05-01 14:38:01 -0700

    Removed nesting functions to keep them separate

--- public/js/ticTacToe.js
+++ public/js/ticTacToe.js
@@ -120,7 +120,7 @@ const onTurnPlayer = (data) => {
 		if (player) {
 			player.turn = true;
 		} else {
-			addSpectatorSigil();
+			addSpectatorSigil(data.key);
 		}
 		usedTiles.push(tilesList[data.key].tile);
 		tilesLib.push({
@@ -133,93 +133,93 @@ const onTurnPlayer = (data) => {
 			checkForWin();
 		}
 	}
+}
 
-	function addSpectatorSigil() {
-		if (tilesList[data.key].coord === 'row2col2') {
-			game.add.sprite(160, 160, 'spectator');
+function addSpectatorSigil(key) {
+	if (tilesList[key].coord === 'row2col2') {
+		game.add.sprite(160, 160, 'spectator');
+	}
+}
+
+function checkForWin() {
+	let colList = ['col1', 'col2', 'col3'];
+	let rowList = ['row1', 'row2', 'row3'];
+	let diagonal1 = ['row1col1', 'row2col2', 'row3col3'];
+	let diagonal2 = ['row3col1', 'row2col2', 'row1col3'];
+	let hasWinner = false;
+
+	if (checkDiagonalWin(diagonal1)) {
+		hasWinner = true;
+		socket.emit('end game', {line: 'lDiag'});
+	}
+	if (checkDiagonalWin(diagonal2)) {
+		hasWinner = true;
+		socket.emit('end game', {line: 'rDiag'});
+	}
+	for (let i=0; i<rowList.length; i++) {
+		if (checkColOrRowWin(rowList[i], 0)) {
+			hasWinner = true;
+			socket.emit('end game', {line: rowList[i]});
+		}
+		if (checkColOrRowWin(colList[i], 4)) {
+			hasWinner = true;
+			socket.emit('end game', {line: colList[i]});
 		}
 	}
 
-	function checkForWin() {
-		let colList = ['col1', 'col2', 'col3'];
-		let rowList = ['row1', 'row2', 'row3'];
-		let diagonal1 = ['row1col1', 'row2col2', 'row3col3'];
-		let diagonal2 = ['row3col1', 'row2col2', 'row1col3'];
-		let hasWinner = false;
+	if (!hasWinner && usedTiles.length === 9) {
+		socket.emit('end game', {line: 'catsGame'})
+	}
+}
 
-		if (checkDiagonalWin(diagonal1)) {
-			hasWinner = true;
-			socket.emit('end game', {line: 'lDiag'});
-		}
-		if (checkDiagonalWin(diagonal2)) {
-			hasWinner = true;
-			socket.emit('end game', {line: 'rDiag'});
-		}
-		for (let i=0; i<rowList.length; i++) {
-			if (checkColOrRowWin(rowList[i], 0)) {
-				hasWinner = true;
-				socket.emit('end game', {line: rowList[i]});
-			}
-			if (checkColOrRowWin(colList[i], 4)) {
-				hasWinner = true;
-				socket.emit('end game', {line: colList[i]});
-			}
-		}
+function checkDiagonalWin(diagonal) {
+	let mark;
+	let colCounter = 0;
 
-		if (!hasWinner && usedTiles.length === 9) {
-			socket.emit('end game', {line: 'catsGame'})
-		}
-		function checkDiagonalWin(diagonal) {
-			let mark;
-			let colCounter = 0;
-
-			for (let i=0; i < diagonal.length; i++) {
-				for (let key in tilesLib) {
-					if(tilesLib[key].coord === diagonal[i] && (!mark || mark === tilesLib[key].mark)) {
-						colCounter++;
-						mark = tilesLib[key].mark;
-					}
-				}
+	for (let i=0; i < diagonal.length; i++) {
+		for (let key in tilesLib) {
+			if(tilesLib[key].coord === diagonal[i] && (!mark || mark === tilesLib[key].mark)) {
+				colCounter++;
+				mark = tilesLib[key].mark;
 			}
-			if (colCounter === 3) {
-				return true;
-			}
-			return false;
-		}
-
-		function checkColOrRowWin (prefix, index) {
-			let mark;
-			let colCounter = 0;
-			for (let key in tilesLib) {
-				if (tilesLib[key].coord.indexOf(prefix) === index && (!mark || mark === tilesLib[key].mark)) {
-					colCounter++;
-					mark = tilesLib[key].mark;
-				}
-			}
-			if (colCounter === 3) {
-				return true;
-			}
-			return false;
 		}
 	}
+	if (colCounter === 3) {
+		return true;
+	}
+	return false;
+}
+
+function checkColOrRowWin (prefix, index) {
+	let mark;
+	let colCounter = 0;
+	for (let key in tilesLib) {
+		if (tilesLib[key].coord.indexOf(prefix) === index && (!mark || mark === tilesLib[key].mark)) {
+			colCounter++;
+			mark = tilesLib[key].mark;
+		}
+	}
+	if (colCounter === 3) {
+		return true;
+	}
+	return false;
 }
 
 const onNewSpectator = (data) => {
 	spectator = data;
 	console.log('Spectator of id ' + data.id + ' has connected');
+
+	renderExistingSprites(data.tilesLib);
+}
+
+function renderExistingSprites(libMap) {
 	let coordMap = buildCoordinates.coordinates;
-	let libMap = data.tilesLib;
-
-	renderExistingSprites();
-
-	function renderExistingSprites() {
-		for (let key in libMap) {
-			tilesLib.push ({
-				markSprite: game.add.sprite(coordMap[key].x+buildCoordinates.offsets[libMap[key]+'OFFSET'], coordMap[key].y+buildCoordinates.offsets[libMap[key]+'OFFSET'],libMap[key]+'Mark')
-			});
-		}
-		game.add.sprite(160, 160, 'spectator');
+	for (let key in libMap) {
+		tilesLib.push ({
+			markSprite: game.add.sprite(coordMap[key].x+buildCoordinates.offsets[libMap[key]+'OFFSET'], coordMap[key].y+buildCoordinates.offsets[libMap[key]+'OFFSET'],libMap[key]+'Mark')
+		});
 	}
+	game.add.sprite(160, 160, 'spectator');
 }
 
 const onClearGame = () => {
@@ -249,33 +249,33 @@ const onClearGame = () => {
 
 const onEndGame = (data) => {
 	renderLine(data.line);
+}
 
-	function renderLine(line) {
-		if (line.indexOf('row') > -1) {
-			let index = line[3];
-			if (player) {
-				player.turn = false;
-			}
-			lines.push(game.add.sprite(0, buildCoordinates.offsets['lineOFFSET'+index], 'hLine'));
+function renderLine(line) {
+	if (line.indexOf('row') > -1) {
+		let index = line[3];
+		if (player) {
+			player.turn = false;
 		}
-		if (line.indexOf('col') > -1) {
-			let index = line[3];
-			if (player) {
-				player.turn = false;
-			}
-			lines.push(game.add.sprite(buildCoordinates.offsets['lineOFFSET'+index], 0, 'vLine'));
+		lines.push(game.add.sprite(0, buildCoordinates.offsets['lineOFFSET'+index], 'hLine'));
+	}
+	if (line.indexOf('col') > -1) {
+		let index = line[3];
+		if (player) {
+			player.turn = false;
 		}
-		if (line === 'lDiag') {
-			if (player) {
-				player.turn = false;
-			}
-			lines.push(game.add.sprite(0, 0, 'lDiag'));
+		lines.push(game.add.sprite(buildCoordinates.offsets['lineOFFSET'+index], 0, 'vLine'));
+	}
+	if (line === 'lDiag') {
+		if (player) {
+			player.turn = false;
 		}
-		if (line === 'rDiag') {
-			if (player) {
-				player.turn = false;
-			}
-			lines.push(game.add.sprite(0, buildCoordinates.offsets.diagOFFSET, 'rDiag'));
+		lines.push(game.add.sprite(0, 0, 'lDiag'));
+	}
+	if (line === 'rDiag') {
+		if (player) {
+			player.turn = false;
 		}
+		lines.push(game.add.sprite(0, buildCoordinates.offsets.diagOFFSET, 'rDiag'));
 	}
 }
